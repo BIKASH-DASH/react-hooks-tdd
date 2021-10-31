655faa2a923ad6900d64d94fe3261be3d0f09bdf
bikash <vikashdash004@gmail.com>
2021-10-31 22:21:32 +0530

    map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.scss';
+import React,{useState} from 'react'
 import NameTag from './components/NameTag';
+
+const initNames = [
+  {firstName:'bikash',lastName:'dash'},
+  {firstName:'pitar',lastName:'parker'},
+  {firstName:'brush',lastName:'wain'},
+]
 function App() {
+  const [user, setUser] = useState(initNames)
   return (
     <div className="App">
       <header className="App-header">
         <h1>Name List</h1>
-        <NameTag firstName="bikash" lastName="dash" ></NameTag>
-        <NameTag  ></NameTag>
+        {user.map((usr,index)=>{
+          return (<NameTag key={index} style={{color:'red'}} firstName={usr.firstName} lastName={usr.lastName} ></NameTag>)
+        })}
+        
       </header>
     </div>
   );
--- src/components/NameTag.js
+++ src/components/NameTag.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+
+
 export default function NameTag(props) {
     return (
-        <div className='name'>
+        <div style={props.style} className='name'>
         <h3 className='nameTag'>{ props.firstName}</h3>
         <h3 className='nameTag'>{ props.lastName}</h3>
         </div>
