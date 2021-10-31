8ee4e416d0dec1e1e59a3d59d5e0eb508e6c10dd
bikash <vikashdash004@gmail.com>
2021-10-31 21:21:29 +0530

    styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 
+const nameStyle = {color:"green",paddingTop:'1px',}
 function App() {
-  console.log('hi');
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Name List</h1>
+        <h3 style={nameStyle}>bikash</h3>
+        <h3>dash</h3>
       </header>
     </div>
   );
